9c80e9dbf1a7781c849a2cd2d313b602aea7a6a8
RetroKevin <RetroKevin@protonmail.com>
2025-01-29 07:03:19 -0600

    3nd commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -122,6 +122,10 @@ const controlAddRecipe = async function (newRecipe) {
   //Upload new recipe data
 }
 
+const gitTest = function () {
+  console.log('This is a GIT test')
+}
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks)
   recipeView.addHandlerRender(controlRecipes)
@@ -130,5 +134,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults)
   paginationView.addHandlerClick(controlPagination)
   addRecipeView.addHandlerUpload(controlAddRecipe)
+  gitTest()
 }
 init()
